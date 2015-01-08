551d2791aad7f6318a5fbf36b4686da47d8608c3
unknown <tei.overgoor@wur.nl>
2015-01-08 16:55:01 +0100

    Finished main script + comments added.

--- main.R
+++ main.R
@@ -1,3 +1,10 @@
 # Team Bastei
 # January 8, 2015
+## Description
+
+source('R/LoadingData.R')     # Loading the two unzipped LANDSAT files provided
+source('R/EqualizeExtent.R')  # Setting files to the same extent
+source('R/CloudMask.R')       # Cloud (shadow, water bodies) masking the red and NIR bands of 1990 and 2014
+source('R/NDVIdifference.R')  # Calculating NDVI images for both years and calculation of the difference.
+
 
